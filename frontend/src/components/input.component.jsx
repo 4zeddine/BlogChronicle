802bef7813fe74872124282148fc5f8f69ffd57d
4zeddine <azeddine.nader@gmail.com>
2024-10-19 22:34:03 +0100

    user auth form component completed commit

--- frontend/src/components/input.component.jsx
+++ frontend/src/components/input.component.jsx
@@ -1,14 +1,21 @@
-const InputComponent = ({id, name, type, value, placeholder}) => {
+import {useState} from "react";
+
+const InputComponent = ({id, name, type, value, placeholder, icon}) => {
+    const [ passwordVisibility , setPasswordVisibility ] = useState(false);
     return(
-        <div className="realtive w-[100%] mb-4">
+        <div className="relative w-[100%] mb-4">
             <input
                 id={id}
                 name={name}
-                type={type}
+                type={type === "password" ? passwordVisibility ? "text" : "password" : type}
                 placeholder={placeholder}
                 defaultValue={value}
                 className="input-box"
             />
+            <i className={"fi "+ icon +" input-icon"}></i>
+            {
+                type === "password" ? <i className={"fi fi-ss-eye"+ (passwordVisibility ? "": "-crossed") +" input-icon left-[auto] right-4 cursor-pointer"} onClick={() => setPasswordVisibility(current => !current )}></i> : ""
+            }
         </div>
     )
 }
--- frontend/src/pages/userAuthForm.page.jsx
+++ frontend/src/pages/userAuthForm.page.jsx
@@ -1,15 +1,37 @@
 import InputComponent from "../components/input.component.jsx";
+import {Link} from "react-router-dom";
+import googleIcon from "../imgs/google.png";
+import PageAnimation from "../common/page-animation.jsx";
 
 const UserAuthFormPage = ({ type }) => {
     return(
-        <section className="h-cover flex items-center justify-center">
-            <form action="" className="w-[80%] max-w-[400px]">
-                <h1 className="text-4xl font-noto capitalize text-center mb-24">
-                    {type === "sign-in" ? "Welcome Back to the blog" : "Create an Account to get started"}
-                </h1>
-                {type !== "sign-in" ? <InputComponent name="full-name" type="text" placeholder="Enter Your Full Name"/> : ""}
-            </form>
-        </section>
+        <PageAnimation>
+            <section className="h-cover flex items-center justify-center">
+                <form action="" className="w-[80%] max-w-[400px]">
+                    <h1 className="text-4xl font-noto capitalize text-center mb-24">
+                        {type === "sign-in" ? "Welcome Back to the blog" : "Create an Account to get started"}
+                    </h1>
+                    {type !== "sign-in" ? <InputComponent name="full-name" type="text" placeholder="Enter Your Full Name" icon="fi-rr-user"/> : ""}
+                    <InputComponent name="email" type="email" placeholder="Enter Your Email " icon="fi-br-envelope"/>
+                    <InputComponent name="password" type="password" placeholder="Enter Your Password" icon="fi-sr-lock"/>
+                    <button className="btn-dark center mt-14" type="submit">
+                        {type.replace("-", " ")}
+                    </button>
+                    <div className="relative w-full flex items-center gap-2 my-10 opacity-10 uppercase text-black font-bold">
+                        <hr className="w-1/2 border-black"/>
+                        <p>Or</p>
+                        <hr className="w-1/2 border-black"/>
+                    </div>
+                    <button className="btn-dark flex items-center justify-center gap-4 w-[80%] center">
+                        <img src={googleIcon} className="w-5" alt="Google logo"/>
+                        Use Google
+                    </button>
+                    {
+                        type === "sign-in" ? <p className="mt-6 text-dark-grey text-xl text-center"> Don't have an account yet? <Link to="/signup" className="underline text-black text-xl ml-1">Join Us</Link></p> : <p className="mt-6 text-dark-grey text-xl text-center"> Already a member? <Link to="/signup" className="underline text-black text-xl ml-1">Sign in here</Link></p>
+                    }
+                </form>
+            </section>
+        </PageAnimation>
     )
 }
 
